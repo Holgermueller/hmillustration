d22c115b0642413da3dc555876656431ac3b71e4
holgermueller <9jholger@gmail.com>
2023-11-25 15:44:25 -0600

    waking this project up

---
+++ app/archives/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArchivesPage = () => {
+  return (
+    <div className="archives">
+      <h2>Archives</h2>
+      <p>Read some older posts</p>
+    </div>
+  );
+};
+
+export default ArchivesPage;
--- app/components/nav.js
+++ app/components/nav.js
@@ -4,6 +4,7 @@ const nav = () => {
   return (
     <nav>
       <Link href="/">Home</Link>
+      <Link href="/archives">Archives</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
     </nav>
