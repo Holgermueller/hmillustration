24e11331e648befd6d91ed39ec820b6e3e8d72d1
holgermueller <9jholger@gmail.com>
2023-11-25 20:06:40 -0600

    added markdown files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,50 @@
-const HomePage = () => {
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const HomePage = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="home">
       <h2>Welcom to H. M. Illustration</h2>
+
+      {/* <div className="posts">
+        {posts.map((post, index) => {
+          <h2 key={index}>{post}</h2>;
+        })}
+      </div> */}
     </div>
   );
 };
 
 export default HomePage;
+
+export async function getInitialProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  console.log(posts);
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    console.log(markdownWithMeta);
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: "the posts",
+    },
+  };
+}
---
+++ posts/test.md
@@ -0,0 +1,13 @@
+---
+title: "Test Post"
+date: "November 25 2023"
+excerpt: "This is the excerpt"
+cover_image: "xxx"
+---
+
+## Heading two
+
+- Item one
+- Item two
+
+Hello world
